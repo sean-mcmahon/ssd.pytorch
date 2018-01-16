a496ae82419771832fff317204c7eed229476f34
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2018-01-16 16:09:14 +1000

    modifying to use my data as well as VOC

--- train.py
+++ train.py
@@ -8,34 +8,55 @@ import argparse
 from torch.autograd import Variable
 import torch.utils.data as data
 from data import v2, v1, AnnotationTransform, VOCDetection, detection_collate, VOCroot, VOC_CLASSES
-from utils.augmentations import SSDAugmentation
+from data.mining import MiningDataset, MiningAnnotationTransform
+from utils.augmentations import SSDAugmentation, SSDMiningAugmentation
 from layers.modules import MultiBoxLoss
 from ssd import build_ssd
 import numpy as np
 import time
 
+
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
 
-parser = argparse.ArgumentParser(description='Single Shot MultiBox Detector Training')
-parser.add_argument('--version', default='v2', help='conv11_2(v2) or pool6(v1) as last layer')
-parser.add_argument('--basenet', default='vgg16_reducedfc.pth', help='pretrained base model')
-parser.add_argument('--jaccard_threshold', default=0.5, type=float, help='Min Jaccard index for matching')
-parser.add_argument('--batch_size', default=16, type=int, help='Batch size for training')
-parser.add_argument('--resume', default=None, type=str, help='Resume from checkpoint')
-parser.add_argument('--num_workers', default=2, type=int, help='Number of workers used in dataloading')
-parser.add_argument('--iterations', default=120000, type=int, help='Number of training iterations')
-parser.add_argument('--start_iter', default=0, type=int, help='Begin counting iterations starting from this value (should be used with resume)')
-parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda to train model')
-parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
+parser = argparse.ArgumentParser(
+    description='Single Shot MultiBox Detector Training')
+parser.add_argument('--version', default='v2',
+                    help='conv11_2(v2) or pool6(v1) as last layer')
+parser.add_argument('--basenet', default='vgg16_reducedfc.pth',
+                    help='pretrained base model')
+parser.add_argument('--jaccard_threshold', default=0.5,
+                    type=float, help='Min Jaccard index for matching')
+parser.add_argument('--batch_size', default=16, type=int,
+                    help='Batch size for training')
+parser.add_argument('--resume', default=None, type=str,
+                    help='Resume from checkpoint')
+parser.add_argument('--num_workers', default=2, type=int,
+                    help='Number of workers used in dataloading')
+parser.add_argument('--iterations', default=120000, type=int,
+                    help='Number of training iterations')
+parser.add_argument('--start_iter', default=0, type=int,
+                    help='Begin counting iterations starting from this value (should be used with resume)')
+parser.add_argument('--cuda', default=True, type=str2bool,
+                    help='Use cuda to train model')
+parser.add_argument('--lr', '--learning-rate', default=1e-3,
+                    type=float, help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
-parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
-parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
-parser.add_argument('--log_iters', default=True, type=bool, help='Print the loss at each iteration')
-parser.add_argument('--visdom', default=False, type=str2bool, help='Use visdom to for loss visualization')
-parser.add_argument('--send_images_to_visdom', type=str2bool, default=False, help='Sample a random image from each 10th batch, send it to visdom after augmentations step')
-parser.add_argument('--save_folder', default='weights/', help='Location to save checkpoint models')
-parser.add_argument('--voc_root', default=VOCroot, help='Location of VOC root directory')
+parser.add_argument('--weight_decay', default=5e-4,
+                    type=float, help='Weight decay for SGD')
+parser.add_argument('--gamma', default=0.1, type=float,
+                    help='Gamma update for SGD')
+parser.add_argument('--log_iters', default=True, type=bool,
+                    help='Print the loss at each iteration')
+parser.add_argument('--visdom', default=False, type=str2bool,
+                    help='Use visdom to for loss visualization')
+parser.add_argument('--send_images_to_visdom', type=str2bool, default=False,
+                    help='Sample a random image from each 10th batch, send it to visdom after augmentations step')
+parser.add_argument('--save_folder', default='weights/',
+                    help='Location to save checkpoint models')
+parser.add_argument('--data_root', default=VOCroot,
+                    help='Location of VOC root directory')
+parser.add_argument('--data_set', default=voc)
 args = parser.parse_args()
 
 if args.cuda and torch.cuda.is_available():
@@ -48,10 +69,7 @@ cfg = (v1, v2)[args.version == 'v2']
 if not os.path.exists(args.save_folder):
     os.mkdir(args.save_folder)
 
-train_sets = [('2007', 'trainval'), ('2012', 'trainval')]
-# train_sets = 'train'
 ssd_dim = 300  # only support 300 now
-means = (104, 117, 123)  # only support voc now
 num_classes = len(VOC_CLASSES) + 1
 batch_size = args.batch_size
 accum_batch_size = 32
@@ -61,6 +79,15 @@ weight_decay = 0.0005
 stepvalues = (80000, 100000, 120000)
 gamma = 0.1
 momentum = 0.9
+
+train_sets = {'voc': [('2007', 'trainval'), ('2012', 'trainval')],
+              'mining': 'train_gopro1_scraped_all_labelled.json'}
+rgb_means = {'voc': (104, 117, 123), 'mining': (65, 69, 76)}
+data_iters = {'voc': VOCDetection, 'mining': MiningDataset}
+augmentators = {'voc': SSDAugmentation(ssd_dim, rgb_means['voc']),
+                'mining': SSDMiningAugmentation(ssd_dim, rgb_means['mining'])}
+target_transforms = {'voc': AnnotationTransform,
+                     'mining': MiningAnnotationTransform}
 
 if args.visdom:
     import visdom
@@ -104,7 +131,8 @@ if not args.resume:
 
 optimizer = optim.SGD(net.parameters(), lr=args.lr,
                       momentum=args.momentum, weight_decay=args.weight_decay)
-criterion = MultiBoxLoss(num_classes, 0.5, True, 0, True, 3, 0.5, False, args.cuda)
+criterion = MultiBoxLoss(num_classes, 0.5, True, 0,
+                         True, 3, 0.5, False, args.cuda)
 
 
 def train():
@@ -115,9 +143,12 @@ def train():
     epoch = 0
     print('Loading Dataset...')
 
-    dataset = VOCDetection(args.voc_root, train_sets, SSDAugmentation(
-        ssd_dim, means), AnnotationTransform())
+    dataset = data_iters[args.data_set](
+        args.data_root, train_sets[args.data_set], augmentators[args.data_set],
+        target_transforms[args.data_set]())
 
+    print('Using "{}" Data'.format(dataset.__class__.__name__))
+    
     epoch_size = len(dataset) // args.batch_size
     print('Training SSD on', dataset.name)
     step_index = 0
@@ -157,7 +188,7 @@ def train():
                 viz.line(
                     X=torch.ones((1, 3)).cpu() * epoch,
                     Y=torch.Tensor([loc_loss, conf_loss,
-                        loc_loss + conf_loss]).unsqueeze(0).cpu() / epoch_size,
+                                    loc_loss + conf_loss]).unsqueeze(0).cpu() / epoch_size,
                     win=epoch_lot,
                     update='append'
                 )
@@ -171,7 +202,8 @@ def train():
 
         if args.cuda:
             images = Variable(images.cuda())
-            targets = [Variable(anno.cuda(), volatile=True) for anno in targets]
+            targets = [Variable(anno.cuda(), volatile=True)
+                       for anno in targets]
         else:
             images = Variable(images)
             targets = [Variable(anno, volatile=True) for anno in targets]
@@ -189,7 +221,8 @@ def train():
         conf_loss += loss_c.data[0]
         if iteration % 10 == 0:
             print('Timer: %.4f sec.' % (t1 - t0))
-            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data[0]), end=' ')
+            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' %
+                  (loss.data[0]), end=' ')
             if args.visdom and args.send_images_to_visdom:
                 random_batch_index = np.random.randint(images.size(0))
                 viz.image(images.data[random_batch_index].cpu().numpy())
@@ -197,7 +230,7 @@ def train():
             viz.line(
                 X=torch.ones((1, 3)).cpu() * iteration,
                 Y=torch.Tensor([loss_l.data[0], loss_c.data[0],
-                    loss_l.data[0] + loss_c.data[0]]).unsqueeze(0).cpu(),
+                                loss_l.data[0] + loss_c.data[0]]).unsqueeze(0).cpu(),
                 win=lot,
                 update='append'
             )
@@ -206,7 +239,7 @@ def train():
                 viz.line(
                     X=torch.zeros((1, 3)).cpu(),
                     Y=torch.Tensor([loc_loss, conf_loss,
-                        loc_loss + conf_loss]).unsqueeze(0).cpu(),
+                                    loc_loss + conf_loss]).unsqueeze(0).cpu(),
                     win=epoch_lot,
                     update=True
                 )
@@ -214,7 +247,8 @@ def train():
             print('Saving state, iter:', iteration)
             torch.save(ssd_net.state_dict(), 'weights/ssd300_0712_' +
                        repr(iteration) + '.pth')
-    torch.save(ssd_net.state_dict(), args.save_folder + '' + args.version + '.pth')
+    torch.save(ssd_net.state_dict(), args.save_folder +
+               '' + args.version + '.pth')
 
 
 def adjust_learning_rate(optimizer, gamma, step):
